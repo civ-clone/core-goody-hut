5880c67bfb82a03f19ff0cb0e7a230f8dfd5c4ef
dom111 <dom111@users.noreply.github.com>
2021-01-10 20:01:26 +0000

    Add `Distribution` `Rule` for controlling placement of huts within `World`s.

---
+++ Rules/Distribution.ts
@@ -0,0 +1,11 @@
+import GoodyHut from '../GoodyHut';
+import { IRuleRegistry } from '@civ-clone/core-rule/RuleRegistry';
+import Rule from '@civ-clone/core-rule/Rule';
+import Unit from '@civ-clone/core-unit/Unit';
+
+export class Distribution extends Rule<[GoodyHut, Unit], void> {}
+
+export default Distribution;
+
+export interface IDistributionRegistry
+  extends IRuleRegistry<Distribution, [GoodyHut, Unit], void> {}
